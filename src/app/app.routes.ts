02aea2618029f6bb463d2d963a4eaec5ef8a881c
“Zahed75” <zahedhasan.cs@gmail.com>
2025-11-30 21:19:17 +0600

    about done

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,11 @@
 // app.routes.ts
 import { Routes } from '@angular/router';
 import { HomePageComponent } from './features/home/components/home-component/home-component';
-
+import { AboutComponent } from './features/about/components/about-component/about-component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomePageComponent },
-  // Add other routes later
+  { path: 'about', component: AboutComponent },
   { path: '**', redirectTo: 'home' }
 ];
