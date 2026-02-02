e3f4a0cbc5a1d1da4e292eb720fc5e8e0bb6a9e5
“Zahed75” <zahedhasan.cs@gmail.com>
2026-02-02 13:08:44 +0600

    production up

--- src/enviornments/enviornment.ts
+++ src/enviornments/enviornment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  // apiUrl: 'https://api.nkscdu.com',
-  apiUrl: 'http://127.0.0.1:8000',
+  apiUrl: 'https://api.nkscdu.com',
+  // apiUrl: 'http://127.0.0.1:8000',
   siteName: 'Nazmul Karim Study Center',
   defaultImage: '/assets/images/og-image.png'
 };
