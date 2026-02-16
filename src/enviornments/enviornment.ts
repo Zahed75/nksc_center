de5c7911cbd0db8d11aefcb99062d378429b9c79
“Zahed75” <zahedhasan.cs@gmail.com>
2026-02-16 06:55:27 +0600

    media updated

--- src/enviornments/enviornment.ts
+++ src/enviornments/enviornment.ts
@@ -1,16 +1,16 @@
-// export const environment = {
-//   production: true,
-//   apiUrl: 'https://api.nkscdu.com',
-//   // apiUrl: 'http://127.0.0.1:8000',
-//   siteName: 'Nazmul Karim Study Center',
-//   defaultImage: '/assets/images/og-image.png'
-// };
-
-
 export const environment = {
   production: true,
-  apiUrl: 'http://127.0.0.1:8000',
+  apiUrl: 'https://api.nkscdu.com',
+  // apiUrl: 'http://127.0.0.1:8000',
   siteName: 'Nazmul Karim Study Center',
   defaultImage: '/assets/images/og-image.png'
 };
+
+
+// export const environment = {
+//   production: true,
+//   apiUrl: 'http://127.0.0.1:8000',
+//   siteName: 'Nazmul Karim Study Center',
+//   defaultImage: '/assets/images/og-image.png'
+// };
 
